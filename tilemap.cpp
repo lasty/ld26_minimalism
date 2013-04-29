d344cb8c35fc39905da35b541c79280ebcb0fd12
Lasty <tristan.lasty@gmail.com>
2013-04-29 15:31:03 +0930

    Abandoned:  Ran out of time (And motivation) [Notes]
    I don't think there is much to salvage or take away from this Ludum
    Dare.
    Everything was quick and dirty, using deprecated opengl.
    I did however try a few new things: exporting image to .c file from
    gimp, and using the Tiled map editor, and hacky json import code.
    Lessons learned:  Get the maths library in faster.  Get the image
    loading in faster.
    Next Time:  I think I will use/publish a base code for SDL and Opengl
    instead of re-re-writing it all from scratch.

--- tilemap.cpp
+++ tilemap.cpp
@@ -20,11 +20,16 @@ bool find(const string &s, const string &f)
 	return s.find(f) != string::npos;
 }
 
+// get the string inbetween two positions
+string GetSubstr(const string &str, size_t p1, size_t p2)
+{
+	return str.substr(p1+1, (p2-p1-1));
+}
+
 // parse the int in the string between the two character positions
 int ParseInt(const string&str, size_t p1, size_t p2)
 {
-	string ss = str.substr(p1+1, (p2-p1-1));
-	//cout << "int = '" << ss << "'" << endl;
+	string ss = GetSubstr(str, p1, p2);
 	stringstream s;  s << ss;
 	int out;
 	s >> out;
@@ -36,14 +41,14 @@ Level::Level(const string&datastr, const string&heightstr)
 	if (not find(datastr, "data")) throw runtime_error("data line not found");
 	if (not find(heightstr, "height")) throw runtime_error("height line not found");
 
-	cout << datastr << endl;
-	cout << heightstr << endl;
+	//cout << datastr << endl;
+	//cout << heightstr << endl;
 
 	size_t h1 = heightstr.find(':');	
 	size_t h2 = heightstr.find(',');
 	height = ParseInt(heightstr, h1, h2);
 
-	cout << "height = " << height << endl;
+	//cout << "height = " << height << endl;
 
 	size_t d1 = datastr.find('[');
 	size_t d2 = datastr.find(',');
@@ -55,18 +60,18 @@ Level::Level(const string&datastr, const string&heightstr)
 		int i = ParseInt(datastr, d1, d2);
 		data.push_back(i);
 
-		cout << i << ",";
+		//cout << i << ",";
 
 		d1 = d2;
 		d2 = datastr.find(',', d2+1);
 	}
 	cout << endl;
 
-	cout << "vector size: " << data.size() << endl;
+	//cout << "vector size: " << data.size() << endl;
 
 	width = data.size() / height;
 
-	cout << "width = " << width << endl;
+	//cout << "width = " << width << endl;
 
 	if (unsigned(width * height) != data.size())
 		throw runtime_error("mismatch in elements");
@@ -105,6 +110,12 @@ void JsonMap::Load(ifstream &in)
 			string height;
 			getline(in, height);
 			
+			string namestr;
+			getline(in, namestr);
+			if (not find(namestr, "\"name\"")) throw runtime_error("name not found");
+			string name = GetSubstr(namestr, namestr.find(':')+1, namestr.find(',')-1);
+			cout << "Level name = '" << name << "'" << endl;
+
 			levels.emplace_back(line, height);
 		}
 	}
